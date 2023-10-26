29755ed7a1e19782e28cdad522fdac94e92aea81
ilyes-hamed <ilyeshamed92@gmail.com>
2023-10-26 17:28:20 +0200

    form

--- error_or_success.php
+++ error_or_success.php
@@ -6,10 +6,8 @@ if (!empty($_POST)) {
         isset($_POST["nom"], $_POST["prenom"], $_POST["mail"], $_POST["message"]) &&
         !empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["mail"] && !empty($_POST["message"]))
     ) {
-
-        echo ("Merci $nom, $prenom, vous êtes bien inscrit !");
-    }
-    else {
-        echo("Désolé, veuillez vérifier vos informations, une erreur a été faite.");
+        header('location: index.php?code=1');
+    } else {
+        header('location: index.php?code=2');
     }
 }
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <div class="contact-box">
             <div class="right">
                 <h2>Contact</h2>
-                <form method="POST" action="verifier_contact.php">
+                <form method="POST" action="error_or_success.php" id="formulaire">
                     <label for="nom"></label>
                     <input type="text" name="nom" id="nom" class="field" placeholder="Nom" required> <br><br>
                     <label for="prenom"></label>
@@ -25,10 +25,24 @@
                     <textarea type="message" name="message" id="message" placeholder="message" class="field"
                         required></textarea>
                     <button type="submit" class="btn" id="bang" name="submit">envoyer</button>
+                    <?php
+
+                            if (isset($_GET['code']) && !empty($_GET['code'])) {
+                                switch ($_GET['code']) {
+                                    case 1: 
+                                        $msg = 'message correct';
+                                        break;
+                                } 
+                                if (isset($msg)) {
+                                    echo '<div class="success" role="success">' . $msg . '</div>';
+                                } 
+                            }
+                            ?>
             </div>
         </div>
     </div>
 
+    <script src="script.js"></script>
 
 </body>
 
